4c35673314e2c060a45fb7f8bf00dfcae3f3ffa2
chase <zijian.shao@hytera.com>
2023-06-06 10:05:55 +0800

    feat: 修改tool文件名

--- add-namespace.js
+++ add-namespace.js
@@ -2,11 +2,12 @@ const postcss = require('postcss')
 const postcssPluginNamespace = require('postcss-plugin-namespace')
 const fs = require('fs')
 
-const fileName = 'bootstrap-cosmo'
+const fileName = 'bootstrap-cosmo'//bootstrap-superhero
+const namespace = 'custom-element-light'//custom-element-dark
 
 var css = fs.readFileSync(`src/${fileName}.css`, 'utf8').toString();
 var out = postcss()
-          .use(postcssPluginNamespace('custom-element-light'))
+          .use(postcssPluginNamespace(namespace))
           .process(css, { from: `src/${fileName}.css`, to: `dist/${fileName}.css` })
           .then(result => {
             fs.writeFile(`dist/${fileName}.css`, result.css, () => true)
